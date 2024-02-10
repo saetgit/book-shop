d3b5945b9cf8dc4dfd10443d4f824c49e0984384
saetgit <etemadian.sara.1998@gmail.com>
2024-02-10 15:11:18 +0330

    upload

---
+++ components/NavBar.vue
@@ -0,0 +1,45 @@
+<template>
+    <header id="header" class="py-5 border-b border-gray-300 bg-gray-100 fixed top-0 right-0 left-0 z-50">
+        <div class="container-fluid">
+            <div class="lg:flex">
+
+                <div class="lg:w-2/12">
+                    <div class="main-logo w-10">
+                        <a href="index.html"><img src="/static/images/main-logo.png" class="w-auto h-auto" alt="logo"></a>
+                    </div>
+                </div>
+
+                <div class="lg:w-10/12">
+
+                    <nav id="navbar">
+                        <div class="main-menu stellarnav flex items-center">
+                            <ul class="menu-list">
+                                <li class="menu-item active inline-block"><a href="#home">Home</a></li>
+                                <li class="menu-item has-sub inline-block">
+                                    <a href="#pages" class="nav-link">Pages</a>
+
+                                    <!-- <ul>
+                                        <li class="active" class="inline-block"><a href="index.html">Home</a></li>
+                                        <li><a href="about.html">About <span class="badge bg-dark">PRO</span></a></li>
+                                        <li><a href="styles.html">Styles <span class="badge bg-dark">PRO</span></a></li>
+                                    
+                                    </ul> -->
+
+                                </li>
+                                <li class="menu-item inline-block"><a href="#featured-books" class="nav-link">Featured</a></li>
+                            </ul>
+
+                            <div class="hamburger">
+                                <span class="bar"></span>
+                                <span class="bar"></span>
+                                <span class="bar"></span>
+                            </div>
+
+                        </div>
+                    </nav>
+
+                </div>
+
+        </div>
+    </div>
+</header></template>
