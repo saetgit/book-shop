67bb9a8898ef2338397e61719064bc9dc1406ec4
saetgit <etemadian.sara.1998@gmail.com>
2024-05-14 16:37:33 +0330

    add pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,27 +4,28 @@
 // nuxt.config.ts
 
 export default defineNuxtConfig({
-  buildModules: [ '@nuxt/http', '@nuxtjs/vuex','@nuxt/auth-next'],
-  modules: ['nuxt-swiper'],
-  auth: {
-    strategies: {
-      local: {
-        token: {
-          property: 'token',
-          required: true,
-          type: 'Bearer',
-        },
-        user: {
-          property: 'user',
-        },
-        endpoints: {
-          login: { url: 'https://reqres.in/api/login', method: 'post' },
-          logout: false,
-          user: false,
-        },
-      },
-    },
-  },
+  // buildModules: [ '@nuxt/http', '@nuxtjs/vuex','@nuxt/auth-next'],
+  modules: ['nuxt-swiper','@pinia/nuxt'],    
+
+  // auth: {
+  //   strategies: {
+  //     local: {
+  //       token: {
+  //         property: 'token',
+  //         required: true,
+  //         type: 'Bearer',
+  //       },
+  //       user: {
+  //         property: 'user',
+  //       },
+  //       endpoints: {
+  //         login: { url: 'https://reqres.in/api/login', method: 'post' },
+  //         logout: false,
+  //         user: false,
+  //       },
+  //     },
+  //   },
+  // },
   http: {
     baseURL: 'http://localhost:3000',
   },
