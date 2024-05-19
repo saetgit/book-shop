adf2a041cb036ce477a050fa4e2c3ae2b081f4a8
saetgit <etemadian.sara.1998@gmail.com>
2024-05-19 15:22:47 +0330

    edit cart page

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,69 +1,63 @@
 <template>
-  <div class="row my-4">
-    <div class="col-md-12">
-      <div class="card">
-        <div class="card-body">
-          <table class="table">
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Image</th>
-                <th>Name</th>
-                <th>Quantity</th>
-                <th>Price</th>
-                <th>Subtotal</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="item in data.getCartItems" :key="item.id">
-                <td>{{ item.id }}</td>
-                <td>
-                  <img
-                    :src="item.picture"
-                    class="fluid rounded"
-                    width="60"
-                    height="60"
-                    :alt="item.name"
-                  />
-                </td>
-                <td>
-                  {{ item.name }}
-                </td>
-                <td>
-                  <i @click="data.incrementQ(item)" class="bi bi-caret-up"></i>
-                  <span class="mx-2">
-                    {{ item.quantity }}
-                  </span>
-                  <i @click="data.decrementQ(item)" class="bi bi-caret-down"></i>
-                </td>
-                <td>${{ item.price }}</td>
-                <td>${{ item.price * item.quantity }}</td>
-                <td>
-                  <i
-                    @click="data.removeFromCart(item)"
-                    class="bi bi-cart-x text-danger"
-                  ></i>
-                </td>
-              </tr>
-              <tr>
-                <th colSpan="3" class="text-center">Total</th>
-                <td colSpan="3" class="text-center">
-                  <span class="badge bg-danger rounded-pill">
-                    ${{
-                      data.cartItems.reduce(
-                        (acc, item) => (acc += item.price * item.quantity),
-                        0
-                      )
-                    }}
-                  </span>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </div>
+  <div class="overflow-x-auto">
+    <table class="min-w-full bg-white">
+      <thead class="bg-gray-800 text-white">
+        <tr>
+          <th class="w-1/12 py-2">#</th>
+          <th class="w-1/12 py-2">عکس محصول</th>
+          <th class="w-3/12 py-2">نام محصول</th>
+          <th class="w-2/12 py-2">تعداد محصول</th>
+          <th class="w-2/12 py-2">قیمت محصول</th>
+          <th class="w-1/12 py-2"></th>
+          <th class="w-2/12 py-2">جمع</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in data.getCartItems" :key="item.id" class="border-b">
+          <td class="py-2 text-center">{{ item.id }}</td>
+          <td class="py-2 text-center">
+            <img :src="item.picture" class="fluid rounded w-14 h-14" :alt="item.name" />
+          </td>
+          <td class="py-2 text-center">{{ item.name }}</td>
+          <td class="py-2 text-center">
+            <button
+              @click="data.incrementQ(item)"
+              class="px-2 py-1 bg-blue-500 text-white rounded"
+            >
+              +
+            </button>
+            <span class="mx-2">{{ item.quantity }}</span>
+            <button
+              @click="data.decrementQ(item)"
+              class="px-2 py-1 bg-red-500 text-white rounded"
+            >
+              -
+            </button>
+          </td>
+          <td class="py-2 text-center">${{ item.price }}</td>
+          <td class="py-2 text-center">
+            <i
+              @click="data.removeFromCart(item)"
+              class="bi bi-cart-x text-red-500 cursor-pointer"
+            ></i>
+          </td>
+          <td class="py-2 text-center">${{ item.price * item.quantity }}</td>
+        </tr>
+        <tr>
+          <th colSpan="5" class="text-center py-2">قیمت کل</th>
+          <td colSpan="2" class="text-center py-2">
+            <span class="badge bg-red-500 text-white rounded-full px-4 py-1">
+              ${{
+                data.cartItems.reduce(
+                  (acc, item) => (acc += item.price * item.quantity),
+                  0
+                )
+              }}
+            </span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
