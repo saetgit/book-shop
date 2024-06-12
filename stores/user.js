d39dca22457bcd51f24f412253be3a96b9bd002b
saetgit <etemadian.sara.1998@gmail.com>
2024-06-12 17:17:25 +0330

    edit user.js

--- stores/user.js
+++ stores/user.js
@@ -1,27 +1,24 @@
 import { defineStore } from 'pinia'
-import { createPersistedState } from 'pinia-plugin-persistedstate'
 
 export const useUserStore = defineStore('user', {
   state: () => ({
     auth:{
       isLoggedIn: false,
-      userInfo: null // اطلاعات کاربر
+      userInfo:{}
     }
   }),
 
+  getters: {
+   
+  },
   actions: {
-    // هنگام ورود کاربر
     logIn(user) {
       this.auth.isLoggedIn = true;
-      this.userInfo = user;
+      this.userInfo = user; 
     },
-    
-    // هنگام خروج کاربر
     logOut() {
       this.auth.isLoggedIn = false;
-      this.userInfo = null;
-    }
+      this.userInfo = null; 
+    },
   },
-
-  plugins: [createPersistedState()],
 })
